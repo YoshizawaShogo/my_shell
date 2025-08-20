52287ef26f7db37973fd656046974417e9e1ad25
Yoshizawa Shogo <shogo.yoshizawa.2000@gmail.com>
2025-08-20 18:16:44 +0900

    練習のためだけのファイルを削除した

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,7 @@
 use my_shell::shell::*;
 
 fn main() {
-    // init
-    // let origin_term = get_term();
-    // set_raw_term(origin_term.clone());
-
     my_shell::init();
     let mut s = MyShell::new();
     s.command_mode();
-
-    // end process
-    // set_term(&origin_term);
 }
